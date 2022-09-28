bbba90907945e7df4e57c8cfdc8e1f77ca02ef20
YuryShageev <schageev.yur@yandex.ru>
2022-09-28 16:30:30 +0300

    Корректировка 2-го блока условных операторов с Бизиным

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,10 @@ public class Main {
     public static void main(String[] args) {
         //Задание 1. Блок 1
         System.out.println("Task 1. Block 1");
-        int yearsOld = 17;
+        int yearsOld = 21;
         if (yearsOld >= 18) {
             System.out.println("Поздравляем с совершеннолетием!");
-        }
-        if (yearsOld < 18) {
+        } else {
             System.out.println("Человек ещё не достиг совершеннолетия");
         }
 
@@ -14,11 +13,9 @@ public class Main {
         System.out.println("Task 2. Block 1");
         if (yearsOld >= 7 && yearsOld < 18) {
             System.out.println("Ребёнок ходит в школу");
-        }
-        if (yearsOld >= 18 && yearsOld < 24) {
+        } else if (yearsOld >= 18 && yearsOld < 24) {
             System.out.println("Человек ходит в университет");
-        }
-        if (yearsOld >= 24) {
+        } else if (yearsOld >= 24) {
             System.out.println("Человек ходит на работу");
         }
 
@@ -28,18 +25,16 @@ public class Main {
         int seatsCount = 60;
         int otherPlaces = capacity - seatsCount;
 
-        int seatsUsed = 13;
-        int otherUsed = 34;
+        int seatsUsed = 60;
+        int otherUsed = 42;
         if (seatsUsed < seatsCount) {
             System.out.println("Есть ещё " + (seatsCount - seatsUsed) + " сидячих мест");
-        }
-        if (seatsUsed == seatsCount) {
+        } else  {
             System.out.println("Сидячих мест нет");
         }
         if (otherUsed < otherPlaces) {
             System.out.println("Есть ещё " + (otherPlaces - otherUsed) + " стоячих мест");
-        }
-        if (otherUsed == otherPlaces) {
+        } else  {
             System.out.println("Стоячих мест нет");
         }
     }
